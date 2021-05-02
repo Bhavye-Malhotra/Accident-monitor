34d3d45f135d22c8fc2e7e6ad7d79fbe5f8e0ffa
Bhavye-Malhotra <killlerinator@gmail.com>
2021-05-02 16:26:12 +0530

    Added all code

--- nearby-hospital-finder/main.py
+++ nearby-hospital-finder/main.py
@@ -1,18 +1,31 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from time import sleep
+import requests as r
 
+name="Ritik"
+carinfo="DLXXXX"
 lat="28.6818237"
 lon="77.1562096"
-
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--incognito")
+speed=120
 
 
-driver = webdriver.Chrome(chrome_options=chrome_options)
-driver.get("https://maps.google.com")
+driver = webdriver.Chrome()
+driver.get(f"https://www.openstreetmap.org/search?query=hopital%20at%20{lat}%2C{lon}")
 
-elem = driver.find_elements_by_id('searchbox')
-elem.send_keys("hospital near " + lat + lon )
-elem.send_keys(Keys.RETURN)
+elem = driver.find_elements_by_xpath("//li")
+#elem = driver.find_element_by_id("query")
+#elem.send_keys(f"{lat},{lon}")
+#elem.send_keys(Keys.RETURN)
+hospital =[]
+for i in elem:
+    data = str(i.text)
+    if "hospital" in (data).lower():
+        hospital.append(data)
+print(hospital)
+
+d = {"name": name, "carinfo":carinfo,"coordinates":lat+','+lon ,"speed":speed }
+response = r.post("http://localhost:5000/users",data=d)
+print(response.text)
 
 #driver.close()
